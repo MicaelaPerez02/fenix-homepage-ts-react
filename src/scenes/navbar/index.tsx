5dbea3b7c5ced9247d9d03d871d7775e70e51239
Micaela Perez <micaelaperez663@gmail.com>
2023-02-02 19:07:44 -0300

    complete navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,20 +4,23 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
-  selectedPage: SelectedPage;
-  setSelectedPage: (value: SelectedPage) => void;
+  isTopOfPage: boolean,
+  selectedPage: SelectedPage,
+  setSelectedPage: (value: SelectedPage) => void,
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
-  const [isMenuToggled, isMenuActive] = useState<boolean>(false);
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             {/* left side */}
@@ -58,7 +61,9 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Registrate</p>
-                  <button>Hacete miembro</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Hacete miembro
+                  </ActionButton>
                 </div>
               </div>
             ) : (
@@ -66,12 +71,54 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                 className="rounded-full bg-secondary-500 p-2"
                 onClick={() => setIsMenuToggled(!isMenuToggled)}
               >
-                <Bars3Icon className="h-6 w-6 text-white"/>
+                <Bars3Icon className="h-6 w-6 text-white" />
               </button>
             )}
           </div>
         </div>
       </div>
+      {/* mobile menu */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* close icon */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          {/* menu items */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                  <Link
+                    page="Inicio"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  >
+                    Inicio
+                  </Link>
+                  <Link
+                    page="Beneficios"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  >
+                    Beneficios
+                  </Link>
+                  <Link
+                    page="Nuestras clases"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  >
+                    Nuestras clases
+                  </Link>
+                  <Link
+                    page="Contactanos"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  >
+                    Contactanos
+                  </Link>
+                </div>
+        </div>
+      )}
     </nav>
   );
 };
