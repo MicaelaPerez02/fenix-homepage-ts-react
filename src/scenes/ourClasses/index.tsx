4fc86615e647501839422c03d0dc75e3b721357f
Micaela Perez <micaelaperez663@gmail.com>
2023-02-09 22:16:29 -0300

    fixing image issue

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,8 +1,8 @@
 import { ClassType, SelectedPage } from "@/shared/types";
-import image1 from "@/assets/image4.jpg";
+import image1 from "@/assets/image1.jpg";
 import image2 from "@/assets/image2.jpg";
 import image3 from "@/assets/image3.jpg";
-import image4 from "@/assets/image1.jpg";
+import image4 from "@/assets/image4.jpg";
 import image5 from "@/assets/image5.jpg";
 import image6 from "@/assets/image6.png";
 import { motion } from "framer-motion";
