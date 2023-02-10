1760f071c9c2c3f26021e342d40f18f756229f01
Micaela Perez <micaelaperez663@gmail.com>
2023-02-09 22:28:33 -0300

    fixing import

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -3,7 +3,7 @@ import image1 from "@/assets/image1.jpg";
 import image2 from "@/assets/image2.jpg";
 import image3 from "@/assets/image3.jpg";
 import image4 from "@/assets/image4.jpg";
-import image5 from "@/assets/image5.jpg";
+import image5 from "@/assets/image55.jpg";
 import image6 from "@/assets/image6.png";
 import { motion } from "framer-motion";
 import HText from "@/shared/HText";
