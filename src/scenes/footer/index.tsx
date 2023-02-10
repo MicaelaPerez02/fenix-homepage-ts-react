9b3a4ea0bfed30f07e7d1efc11819fad2efea381
Micaela Perez <micaelaperez663@gmail.com>
2023-02-09 22:01:40 -0300

    fixing errors

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,11 +1,8 @@
-import { SelectedPage } from "@/shared/types";
 import Logo from "@/assets/Logo.png";
 
-type Props = {
-  setSelectedPage: (value: SelectedPage) => void;
-};
+type Props = {};
 
-const Footer = ({ setSelectedPage }: Props) => {
+const Footer = (props: Props) => {
   return (
     <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
@@ -17,25 +14,25 @@ const Footer = ({ setSelectedPage }: Props) => {
           <p>© Fenix Centro de Entrenamiento. All rights reserved </p>
         </div>
         <div className="mt-10 basis-1/4 md:mt-0">
-          <h4 className="font-bold py-2" >Links</h4>
+          <h4 className="py-2 font-bold">Links</h4>
           <a
             target="_blank"
-            className="my-5 text-decoration-none"
+            className="text-decoration-none my-5"
             href="https://www.instagram.com/fenix_entrenamiento_/"
           >
             Instagram
           </a>
-         <p className="py-2"></p> 
+          <p className="py-2"></p>
           <a
             target="_blank"
-            className="my-5 text-decoration-none"
+            className="text-decoration-none my-5"
             href="https://goo.gl/maps/hzxKGd6ZKAdbqRDy8"
           >
             Mapa
           </a>
         </div>
         <div className="mt-10 basis-1/4 md:mt-0">
-          <h4 className="font-bold pt-2">Contactanos</h4>
+          <h4 className="pt-2 font-bold">Contactanos</h4>
           <p className="my-5">+54 11 2184-6416</p>
           <p className="my-5">
             Emilio Mitre 836, Ramos Mejia, Provincia de Buenos Aires
