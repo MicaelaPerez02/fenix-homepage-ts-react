c1f4e67aa7a3cb5f2c26eed38bbc6ddaf586194f
Micaela Perez <micaelaperez663@gmail.com>
2023-02-09 22:26:04 -0300

    another import failing

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,5 +1,5 @@
 import { ClassType, SelectedPage } from "@/shared/types";
-import image1 from "@/assets/image11.jpg";
+import image1 from "@/assets/image1.jpg";
 import image2 from "@/assets/image2.jpg";
 import image3 from "@/assets/image3.jpg";
 import image4 from "@/assets/image4.jpg";
