0df1bfe79c2161e2c69284e87a2325f9599fae96
Micaela Perez <micaelaperez663@gmail.com>
2023-02-09 23:27:05 -0300

    trying to fix import images

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,10 +1,10 @@
 import { ClassType, SelectedPage } from "@/shared/types";
-import image1 from "@/assets/image1.jpg";
-import image2 from "@/assets/image2.jpg";
-import image3 from "@/assets/image3.jpg";
-import image4 from "@/assets/image4.jpg";
-import image5 from "@/assets/image55.jpg";
-import image6 from "@/assets/image6.png";
+import image1 from "../../assets/image1.jpg"
+import image2 from "../../assets/image2.jpg";
+import image3 from "../../assets/image3.jpg";
+import image4 from "../../assets/image4.jpg";
+import image5 from "../../assets/image55.jpg";
+import image6 from "../../assets/image6.png";
 import { motion } from "framer-motion";
 import HText from "@/shared/HText";
 import Class from "./Class";
